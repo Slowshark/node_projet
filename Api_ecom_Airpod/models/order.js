99474265d5df8eadf4f5c96b0a6586ac94a8557b
YvanU <ung.thivong91@gmail.com>
2024-12-19 16:50:15 +0100

    modif

--- Api_ecom_Airpod/models/order.js
+++ Api_ecom_Airpod/models/order.js
@@ -3,12 +3,28 @@ module.exports = (sequelize, DataTypes) => {
   const Order = sequelize.define('Order', {
     userId: DataTypes.INTEGER,
     date_commande: DataTypes.DATE,
-    statut: DataTypes.STRING,
-    total: DataTypes.DECIMAL,
+    statut: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'En attente'
+    },
+    total: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    shipping_address: DataTypes.TEXT,
+    payment_method_id: DataTypes.INTEGER,
+    shipping_status_id: DataTypes.INTEGER,
   }, {});
+  
   Order.associate = function(models) {
     Order.belongsTo(models.User, { foreignKey: 'userId' });
     Order.hasMany(models.OrderItem, { foreignKey: 'commandeId' });
+    Order.hasMany(models.Payment, { foreignKey: 'orderId' });
+    Order.hasMany(models.Order_Promotion, { foreignKey: 'orderId' });
+    Order.belongsTo(models.Payment_Method, { foreignKey: 'payment_method_id' });
+    Order.belongsTo(models.Shipping_Status, { foreignKey: 'shipping_status_id' });
   };
+  
   return Order;
 };
--- Api_ecom_Airpod/models/user.js
+++ Api_ecom_Airpod/models/user.js
@@ -2,13 +2,29 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     nom: DataTypes.STRING,
-    email: DataTypes.STRING,
-    mot_de_passe: DataTypes.STRING,
-    role: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true }
+    },
+    mot_de_passe: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'customer'
+    },
   }, {});
+  
   User.associate = function(models) {
     User.hasMany(models.Order, { foreignKey: 'userId' });
     User.hasOne(models.Cart, { foreignKey: 'userId' });
+    User.hasMany(models.Product_Review, { foreignKey: 'userId' });
+    User.hasMany(models.User_Token, { foreignKey: 'userId' });
   };
+  
   return User;
 };
