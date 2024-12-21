53cf801417938b7c6052f85e8d275dcc30eee7b1
BixTheOne <153998619+BixTheOne@users.noreply.github.com>
2024-12-21 22:51:24 +0100

    Update authenticateJWT.js sécurité

--- middlewares/authenticateJWT.js
+++ middlewares/authenticateJWT.js
@@ -1,6 +1,6 @@
-// middlewares/authenticateJWT.js
 'use strict';
 const jwt = require('jsonwebtoken');
+const logger = require('../utils/logger');
 require('dotenv').config();
 
 const authenticateJWT = (req, res, next) => {
@@ -9,14 +9,15 @@ const authenticateJWT = (req, res, next) => {
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) {
-        console.error('Erreur JWT:', err);
-        return res.sendStatus(403); // Forbidden
+        logger.error('JWT verification failed:', err.message);
+        return res.status(403).json({ message: 'Accès refusé. Token invalide.' });
       }
       req.user = user;
       next();
     });
   } else {
-    res.sendStatus(401); // Unauthorized
+    logger.warn('JWT missing in request headers.');
+    res.status(401).json({ message: 'Accès non autorisé. Token manquant.' });
   }
 };
 
