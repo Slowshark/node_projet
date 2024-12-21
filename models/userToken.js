d56f2bee49bbd83a7bff9cc0139b8c1bd744e374
BixTheOne <153998619+BixTheOne@users.noreply.github.com>
2024-12-21 23:06:28 +0100

    Update userToken.js

--- models/userToken.js
+++ models/userToken.js
@@ -1,33 +1,34 @@
 'use strict';
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs'); // Module pour le hachage des données sensibles
 
 module.exports = (sequelize, DataTypes) => {
   const User_Token = sequelize.define('User_Token', {
     userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+      type: DataTypes.INTEGER, // ID de l'utilisateur
+      allowNull: false, // Champ obligatoire
     },
     token: {
-      type: DataTypes.TEXT,
-      allowNull: false
+      type: DataTypes.TEXT, // Token d'accès
+      allowNull: false, // Champ obligatoire
     },
     issued_at: {
-      type: DataTypes.DATE,
-      allowNull: false
+      type: DataTypes.DATE, // Date de création du token
+      allowNull: false,
     },
     expires_at: {
-      type: DataTypes.DATE,
-      allowNull: false
-    }
+      type: DataTypes.DATE, // Date d'expiration du token
+      allowNull: false,
+    },
   }, {});
 
+  // Déclaration des associations
   User_Token.associate = function(models) {
-    User_Token.belongsTo(models.User, { foreignKey: 'userId' });
+    User_Token.belongsTo(models.User, { foreignKey: 'userId' }); // Association avec le modèle User
   };
 
-  // Hash token before saving
+  // Hook pour hacher les tokens avant de les enregistrer
   User_Token.beforeCreate(async (tokenInstance) => {
-    tokenInstance.token = await bcrypt.hash(tokenInstance.token, 10);
+    tokenInstance.token = await bcrypt.hash(tokenInstance.token, 10); // Hachage du token
   });
 
   return User_Token;
