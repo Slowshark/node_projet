fecb8b85b1a5dee82cc415f4f746275026c12de9
YvanU <97989887+Slowshark@users.noreply.github.com>
2024-12-21 15:42:05 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,       // 'YvanU'
-    password: process.env.DB_PASSWORD,   // 'YvanU'
-    database: process.env.DB_NAME,       // 'bdd_node_shop'
-    host: process.env.DB_HOST,           // 'localhost'
-    port: process.env.DB_PORT,           // 5432
-    dialect: process.env.DB_DIALECT,     // 'postgres'
-    logging: false,                      // Désactive les logs SQL (optionnel)
+    username: process.env.DB_USER,      
+    password: process.env.DB_PASSWORD,  
+    database: process.env.DB_NAME,       
+    host: process.env.DB_HOST,          
+    port: process.env.DB_PORT,           
+    dialect: process.env.DB_DIALECT,     
+    logging: false,                     
   },
   test: {
     username: process.env.DB_USER,
